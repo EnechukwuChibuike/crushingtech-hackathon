7da423d2d0e3639df606676f7a8e56a29541ac09
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2023-11-25 15:53:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const notification_dropdown = document.querySelector(".notification_dropdown");
+const notification = document.querySelector(".notification");
+const profileName = document.querySelector(".profileName");
+const profileName_dropdown = document.querySelector(".profileName_dropdown");
+
+function displayNotificationDropdown() {
+  if (notification_dropdown.style.display === "flex") {
+    notification_dropdown.style.display = "none";
+    notification.style.background = "#303030";
+  } else {
+    notification_dropdown.style.display = "flex";
+    notification.style.background = "#616161";
+  }
+}
+
+function displayProfileDropdown() {
+  if (profileName_dropdown.style.display === "block") {
+    profileName_dropdown.style.display = "none";
+    profileName.style.background = "#303030";
+  } else {
+    profileName_dropdown.style.display = "block";
+    profileName.style.background = "#616161";
+  }
+}
